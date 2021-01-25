bd3372a1a2e2e982e9d4d0976679280931f35141
Dagwood <sudhagarnarayanan@gmail.com>
2021-01-25 16:20:05 +0530

    api checking

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
-
+@api_view(['GET'])
 def index(request):
     """
         Index for the app
     """
-    return HttpResponse(f'<h1> Hello World </h1>')
+    return Response({'data': 'Hello World'}, status=status.HTTP_200_OK)
